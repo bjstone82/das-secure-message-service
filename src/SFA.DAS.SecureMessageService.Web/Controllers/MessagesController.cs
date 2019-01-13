fde74f601083e4c8019cbcbea485c973dfaef291
Craig Gumbley <craiggumbley@gmail.com>
2019-01-13 21:44:13 +0000

    do not allow empty messages

--- src/SFA.DAS.SecureMessageService.Web/Controllers/MessagesController.cs
+++ src/SFA.DAS.SecureMessageService.Web/Controllers/MessagesController.cs
@@ -26,7 +26,13 @@ namespace SFA.DAS.SecureMessageService.Web.Controllers
             var ttl = Convert.ToInt32(Request.Form["TtlValue.Keys"]);
 
             // Create the message and retrieve the url token
-            var key = await messageService.Create(Request.Form["Message"], ttl );
+            var message = Request.Form["Message"];
+
+            if (String.IsNullOrEmpty(message)){
+                throw new Exception("Message cannot be null");
+            }
+
+            var key = await messageService.Create(message, ttl );
             var url = $"{Request.Scheme}://{Request.Host}/messages/{key}";
 
             var showMessageUrlViewModel = new ShowMessageUrlViewModel() { Url = url };
