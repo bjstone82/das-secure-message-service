9592318a60a929c814edcf05edcc908b2e4926db
EwanNoble <ewan.noble@education.gov.uk>
2019-01-28 14:10:29 +0000

    Add unit test for message controller

--- src/SFA.DAS.SecureMessageService.Web.UnitTests/MessagesControllerTests.cs
+++ src/SFA.DAS.SecureMessageService.Web.UnitTests/MessagesControllerTests.cs
@@ -1,3 +1,9 @@
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
@@ -17,6 +23,38 @@ namespace SFA.DAS.SecureMessageService.Web.UnitTests
         {
             logger = new Mock<ILogger<MessagesController>>();
             messageService = new Mock<IMessageService>();
+        }
+
+        [Test]
+        public async Task Messages_SuccessfullySavesAValidForm()
+        {
+            var controller = new MessagesController(messageService.Object, logger.Object);
+            var mockHttpContext = new Mock<HttpContext>();
+            var mockRequest = new Mock<HttpRequest>();
+
+            mockRequest.SetupGet(t => t.Form["TtlValue.Keys"]).Returns("1");
+            mockRequest.SetupGet(t => t.Form["Message"]).Returns("testmessage");
+            mockRequest.SetupGet(t => t.Scheme).Returns("https");
+            mockRequest.SetupGet(t => t.Host).Returns(new HostString("localhost", 1234));
+
+            messageService.Setup(c => c.Create("testmessage", 1)).ReturnsAsync("somekey1234");
+
+            mockHttpContext.SetupGet(h => h.Request).Returns(mockRequest.Object);
+
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = mockHttpContext.Object
+            };
+
+            var result = await controller.SaveMessage();
+
+            Assert.IsNotNull(result);
+            var viewResult = result as ViewResult;
+            Assert.IsNotNull(viewResult);
+            Assert.AreEqual(viewResult.ViewName, "ShowMessageUrl");
+            var model = viewResult.Model as ShowMessageUrlViewModel;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(model.Url, "https://localhost:1234/messages/somekey1234");
         }
     }
 }
