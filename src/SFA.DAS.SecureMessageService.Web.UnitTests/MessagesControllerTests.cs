589b122f4528a8b38098021e023754827f8d12f6
Craig Gumbley <craiggumbley@gmail.com>
2019-01-31 22:09:21 +0000

    refactor and add additional tests

--- src/SFA.DAS.SecureMessageService.Web.UnitTests/MessagesControllerTests.cs
+++ src/SFA.DAS.SecureMessageService.Web.UnitTests/MessagesControllerTests.cs
@@ -21,8 +21,11 @@ namespace SFA.DAS.SecureMessageService.Web.UnitTests
         protected Mock<ILogger<MessagesController>> logger;
         protected Mock<IMessageService> messageService;
         protected ControllerContext controllerContext;
+        protected MessagesController controller;
         protected string testHttpScheme = "https";
         protected string testHostname = "localhost";
+        protected string testKey = "36ae5a4a-068c-450a-9edf-f5f56d74f13e";
+        protected string testMessage = "test message";
         protected int testPort = 1234;
 
         [SetUp]
@@ -39,18 +42,16 @@ namespace SFA.DAS.SecureMessageService.Web.UnitTests
             {
                 HttpContext = mockHttpContext.Object
             };
+            controller = new MessagesController(messageService.Object, logger.Object);
+            controller.ControllerContext = controllerContext;
+
         }
 
         [Test]
-        public async Task Messages_SuccessfullyRetrievesMessageUrlWhenMessageExists()
+        public async Task ShareMessageUrl_SuccessfullyRetrievesMessageUrlWhenMessageExists()
         {
             // Arrange
-            var testKey = "sometestkey5312";
             messageService.Setup(e => e.MessageExists(testKey)).ReturnsAsync(true);
-
-            var controller = new MessagesController(messageService.Object, logger.Object);
-            controller.ControllerContext = controllerContext;
-
 
             // Act
             var result = await controller.ShareMessageUrl(testKey);
@@ -60,6 +61,71 @@ namespace SFA.DAS.SecureMessageService.Web.UnitTests
             Assert.IsNotNull(actualResult);
             Assert.AreEqual(typeof(ShowMessageUrlViewModel), actualResult.Model.GetType());
             Assert.AreEqual(((ShowMessageUrlViewModel)actualResult.Model).Url, $"{testHttpScheme}://{testHostname}:{testPort}/messages/{testKey}");
+            messageService.VerifyAll();
+        }
+
+        [Test]
+        public async Task ShareMessageUrl_ReturnsInvalidMessageKeyViewWhenInvalidKeyIsPassed()
+        {
+            // Arrange
+            messageService.Setup(e => e.MessageExists(testKey)).ReturnsAsync(false);
+
+            // Act
+            var result = await controller.ShareMessageUrl(testKey);
+
+            // Assert
+            var actualResult = result as ViewResult;
+            Assert.IsNotNull(actualResult);
+            Assert.AreEqual("InvalidMessageKey", actualResult.ViewName);
+            messageService.VerifyAll();
+        }
+
+        [Test]
+        public async Task ConfirmViewMessage_DisplaysTheCorrectViewWhenRetrievingAMessageAndItDoesExist()
+        {
+            // Arrange
+            messageService.Setup(e => e.MessageExists(testKey)).ReturnsAsync(true);
+
+            // Act
+            var result = await controller.ConfirmViewMessage(testKey);
+
+            // Assert
+            var actualResult = result as ViewResult;
+            Assert.AreEqual("ConfirmViewMessage", actualResult.ViewName);
+            Assert.AreEqual(true, actualResult.ViewData["MessageExists"]);
+            messageService.VerifyAll();
+        }
+
+        [Test]
+        public async Task ConfirmViewMessage_DisplaysTheCorrectViewWhenRetrievingAMessageAndItDoesNotExist()
+        {
+            // Arrange
+            messageService.Setup(e => e.MessageExists(testKey)).ReturnsAsync(false);
+
+            // Act
+            var result = await controller.ConfirmViewMessage(testKey);
+
+            // Assert
+            var actualResult = result as ViewResult;
+            Assert.AreEqual("ConfirmViewMessage", actualResult.ViewName);
+            Assert.AreEqual(false, actualResult.ViewData["MessageExists"]);
+            messageService.VerifyAll();
+        }
+
+        [Test]
+        public async Task ViewMessage_DisplaysAMessageAfterConfirmationWhenItExists()
+        {
+            // Arrange
+            messageService.Setup(e => e.Retrieve(testKey)).ReturnsAsync(testMessage);
+
+            // Act
+            var result = await controller.ViewMessage(testKey);
+
+            // Assert
+            var actualResult = result as ViewResult;
+            Assert.AreEqual(typeof(ViewMessageViewModel), actualResult.Model.GetType());
+            Assert.AreEqual(((ViewMessageViewModel)actualResult.Model).Message, testMessage);
+            messageService.VerifyAll();
         }
     }
 }
